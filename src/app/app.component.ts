1348797f49f333e3406eb72fd07987ffbcc6414a
Juri <juri.strumpflohner@gmail.com>
2018-01-19 16:03:10 +0100

    use environment file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="text-align:center">
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+  `
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    this.title = environment.appTitle;
+  }
 }
